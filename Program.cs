a3f7513f699838f9fbdad52656c6c25589f7d5b2
anishvincent05 <anishvincent@outlook.com>
2024-04-02 21:48:35 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace car_insurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   // Taking details of customer
+            Console.WriteLine("What is your age?"); 
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI?");
+            string stDUI = Console.ReadLine();
+            bool dui = stDUI == "Yes"; 
+            Console.WriteLine("How many speeding tickets do you have?");
+            int speed = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(age>15 && dui==false && speed<=3);// checking conditions and giving output
+
+
+        }
+    }
+}
